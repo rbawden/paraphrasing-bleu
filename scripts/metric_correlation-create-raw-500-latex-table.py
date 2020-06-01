106953223bc154a09986e72742c7847ccb864fa3
Rachel Bawden <rbawden@baldur.inf.ed.ac.uk>
2020-06-01 09:50:23 +0000

    added latex create table for subset

---
+++ scripts/metric_correlation-create-raw-500-latex-table.py
@@ -0,0 +1,123 @@
+#!/bin/python
+import metric_correlation_syslevel as mcsys
+import metric_correlation_seglevel as mcseg
+import os
+
+def get_all_lang_correlations(mc, baseline, gold, system):
+    # get correlation results
+    baseline_results, system_results, sigs = mc.get_results(gold, baseline, system)
+
+    assert len(system_results) == 1 and 'de-en' in system_results
+    
+    sig_txt = '' if sigs['de-en'] == '' else r'\textbf'
+            
+    print(sig_txt + '{%.2f} ' % system_results['de-en'][0]), end=' ')
+            
+
+
+def write_table(metric):
+
+    thisdir=os.path.dirname(os.path.abspath(__file__)) + '/'
+        if metric == 'bleu':
+            baseline = thisdir + '../metric-scores/' + testset + '/sacreBLEU'
+        else:
+            baseline = thisdir + '../metric-scores/' + testset + '/Meteor'
+        system_prefix = thisdir + '../metric-scores/' + testset + '/'
+        gold_sys = thisdir + '../metrics-task/DA-syslevel-' + testset + '.csv'
+        gold_seg = thisdir + '../metrics-task/RR-seglevel-' + testset + '.500.csv'
+        
+    print(r'\centering\small')
+    print(r'\resizebox{\linewidth}{!}{')
+    print(r'\begin{tabular}{llll}')
+    print(r'\toprule')
+    print(r'&& \multicolumn{2}{c}{Correlation} \\')
+    print(r'& Method & System & Segment \\')
+
+    # baseline
+    print(r'\midrule')
+    print(r'Baseline & (sentence)' + '\\' +metric, end=' & ')
+    get_all_lang_correlations(mcsys, baseline + '-syslevel.500.tsv', gold_sys, baseline + '-syslevel.500.tsv')
+    print(r' & ', end=' ')
+    get_all_lang_correlations(mcsys, baseline + '-seglevel.500.tsv', gold_seg, baseline + '-seglevel.500.tsv')
+    print(r'\\')
+
+    # Paraphrase baselines
+    print(r'\midrule')
+    print(r'\multirow{3}{*}{\pbox{1.3cm}{Baselines (+5)}}')
+    print(r'\beam & ', end=' ')
+    get_all_lang_correlations(mcsys, baseline + '-syslevel.500.tsv', gold_sys, system_prefix + '/beam/par' + metric + '-beam.num=5-syslevel.500.tsv')
+    print(r' & ', end=' ')
+    get_all_lang_correlations(mcseg, baseline + '-seglevel.500.tsv', gold_seg, system_prefix + '/beam/par' + metric + '-beam.num=5-seglevel.500.tsv')
+
+    print(r'\random & ', end=' ')
+    get_all_lang_correlations(mcsys, baseline + '-syslevel.500.tsv', gold_sys, system_prefix + '/random/par' + metric + '-random.num=5-syslevel.500.tsv')
+    print(r' & ', end=' ')
+    get_all_lang_correlations(mcseg, baseline + '-seglevel.500.tsv', gold_seg, system_prefix + '/random/par' + metric + '-random.num=5-seglevel.500.tsv')
+
+    print(r'\sampled & ', end=' ')
+    get_all_lang_correlations(mcsys, baseline + '-syslevel.500.tsv', gold_sys, system_prefix + '/sampled/par' + metric + '-sampled.num=5-syslevel.500.tsv')
+    print(r' & ', end=' ')
+    get_all_lang_correlations(mcseg, baseline + '-seglevel.500.tsv', gold_seg, system_prefix + '/sampled/par' + metric + '-sampeled.num=5-seglevel.500.tsv')
+
+    # Diversity (+1)
+    print(r'\midrule')
+    print(r'\multirow{2}{*}{\pbox{1.3cm}{Diversity (+1)}}')
+    print(r'\laser & ', end=' ')
+    get_all_lang_correlations(mcsys, baseline + '-syslevel.500.tsv', gold_sys, system_prefix + '/laser/par' + metric + '-laser.num=1-syslevel.500.tsv')
+    print(r' & ', end=' ')
+    get_all_lang_correlations(mcseg, baseline + '-seglevel.500.tsv', gold_seg, system_prefix + '/laser/par' + metric + '-laser.num=1-seglevel.500.tsv')
+
+    print(r'\treelstm & ', end=' ')
+    get_all_lang_correlations(mcsys, baseline + '-syslevel.500.tsv', gold_sys, system_prefix + '/treelstm/par' + metric + '-treelstm.num=1-syslevel.500.tsv')
+    print(r' & ', end=' ')
+    get_all_lang_correlations(mcseg, baseline + '-seglevel.500.tsv', gold_seg, system_prefix + '/treelstm/par' + metric + '-treelstm.num=1-seglevel.500.tsv')
+
+    # Diversity (+5)
+    print(r'\midrule')
+    print(r'\multirow{2}{*}{\pbox{1.3cm}{Diversity (+5)}}')
+    print(r'\laser & ', end=' ')
+    get_all_lang_correlations(mcsys, baseline + '-syslevel.500.tsv', gold_sys, system_prefix + '/laser/par' + metric + '-laser.num=5-syslevel.500.tsv')
+    print(r' & ', end=' ')
+    get_all_lang_correlations(mcseg, baseline + '-seglevel.500.tsv', gold_seg, system_prefix + '/laser/par' + metric + '-laser.num=5-seglevel.500.tsv')
+
+    print(r'\treelstm & ', end=' ')
+    get_all_lang_correlations(mcsys, baseline + '-syslevel.500.tsv', gold_sys, system_prefix + '/treelstm/par' + metric + '-treelstm.num=5-syslevel.500.tsv')
+    print(r' & ', end=' ')
+    get_all_lang_correlations(mcseg, baseline + '-seglevel.500.tsv', gold_seg, system_prefix + '/treelstm/par' + metric + '-treelstm.num=5-seglevel.500.tsv')
+
+
+    # Output-specific (+1)
+    print(r'\midrule')
+    print(r'\multirow{2}{*}{\pbox{1.3cm}{Output-specific (+1)}}')
+    print(r'\laser & ', end=' ')
+    get_all_lang_correlations(mcsys, baseline + '-syslevel.500.tsv', gold_sys, system_prefix + '/mt-output-specific/par' + metric + 'constrained-laser.syslevel.500.tsv')
+    print(r' & ', end=' ')
+    get_all_lang_correlations(mcseg, baseline + '-seglevel.500.tsv', gold_seg, system_prefix + '/mt-output-specific/par' + metric + 'constrained-laser.seglevel.500.tsv')
+
+    print(r'\treelstm & ', end=' ')
+    get_all_lang_correlations(mcsys, baseline + '-syslevel.500.tsv', gold_sys, system_prefix + '/mt-output-specific/par' + metric + 'constrained-treelstm.syslevel.500.tsv')
+    print(r' & ', end=' ')
+    get_all_lang_correlations(mcseg, baseline + '-seglevel.500.tsv', gold_seg, system_prefix + '/mt-output-specific/par' + metric + 'constrained-treelstm.seglevel.500.tsv')
+
+    # Constrained outputs (calculated externally)
+    print(r'\midrule')
+    print(r'\multirow{1}{*}{Constraints}')
+    print(r'& 4-gram & 0.868 & 0.061 \\') 
+    
+    # Human results
+    print(r'Human & ', end=' ')
+    get_all_lang_correlations(mcsys, baseline + '-syslevel.500.tsv', gold_sys, system_prefix + '/human/par' + metric + '-human-syslevel.500.tsv')
+    print(r' & ', end=' ')
+    get_all_lang_correlations(mcseg, baseline + '-seglevel.500.tsv', gold_seg, system_prefix + '/human/par' + metric + '-human-seglevel.500.tsv')
+    print(r'\\')
+
+
+    # Footer
+    print(r'\bottomrule')
+    print(r'\end{tabular}}')
+    print(r'\caption{Correlations on the 500-sentence subset.}')
+    print(r'\label{tab:subset-seg-level}')
+    
+if __name__ == '__main__':
+
+    write_table('bleu')
--- scripts/metric_correlation-create-raw-latex-table.py
+++ scripts/metric_correlation-create-raw-latex-table.py
@@ -12,13 +12,12 @@ def get_all_lang_correlations(mc, baseline, gold, system):
     for lp in sorted(list(system_results.keys())):
         sig_txt = '' if sigs[lp] == '' else r'\textbf'
         score = system_results[lp][0]
-        lp_scores.append(sig_txt + "{%.2f}" % score)
+        lp_scores.append(sig_txt + "{%.3f}" % score)
 
     # average of all
     ave = sum([system_results[lp][0] for lp in system_results]) / len(system_results)
-    lp_scores.append('%.2f' % ave)
+    lp_scores.append('%.3f' % ave)
     print(' & '.join(lp_scores) + r' \\')
-
 
 def write_table_content(testset, metric, level):
 
@@ -33,17 +32,31 @@ def write_table_content(testset, metric, level):
     gold_sys = thisdir + '../metrics-task/DA-syslevel-' + testset + '.csv'
     gold_seg = thisdir + '../metrics-task/RR-seglevel-' + testset + '.csv'
     gold = gold_sys if level == 'sys' else gold_seg
+
+    # list of languages
+    langs = {}
+    with open(baseline) as fp:
+        for line in fp:
+            lang = line.split()[1]
+            lang.replace('-', '--')
+            if '-en' in lang and lang not in langs:
+                langs[lang] = 0
+            if '-en' in lang:
+                langs[lang] += 1
+
+    print(' && ' + ' & '.join(sorted(list(langs.keys()))) + r' & Ave \\')
+    print(' Approach & Method & (' + ') & ('.join([str(langs[x]) for x in sorted(list(langs.keys()))]) + r'\\')
     
     # baseline
     if level == 'sys':
-        print(r'\midrule' + '\n' + r'Baseline & ' + metric + r' & ', end=' ')
+        print(r'\midrule' + '\n' + r'Baseline & \\' + metric + r' & ', end=' ')
     else:
-        print(r'\midrule' + '\n' + r'Baseline & sentence' + metric + r' & ', end=' ')
+        print(r'\midrule' + '\n' + r'Baseline & sentence\\' + metric + r' & ', end=' ')
     system = baseline
     get_all_lang_correlations(mc, baseline, gold, system)
 
     # paraphrase baselines (+5)
-    print(r'\midrule' + '\n' + r'\multirow{3}{*}{\pbox{1.5cm}{Paraphrase baselines (+5)}}')
+    print(r'\midrule' + '\n' + r'\multirow{3}{*}{\pbox{1.9cm}{Paraphrase baselines (+5)}}')
     system = system_prefix + '/beam/par' + metric + '-beam.num=5-' + level + 'level.tsv'
     print(r' & \beam ', end=' & ')
     get_all_lang_correlations(mc, baseline, gold, system)
@@ -55,7 +68,7 @@ def write_table_content(testset, metric, level):
     get_all_lang_correlations(mc, baseline, gold, system)
     
     # diversity (+1)
-    print(r'\midrule' + '\n' + r'\multirow{3}{*}{\pbox{1.5cm}{Diversity (+1)}}')
+    print(r'\midrule' + '\n' + r'\multirow{2}{*}{\pbox{1.9cm}{Diversity (+1)}}')
     system = system_prefix + '/laser/par' + metric + '-laser.num=1-' + level + 'level.tsv'
     print(r' & \laser ', end=' & ')
     get_all_lang_correlations(mc, baseline, gold, system)
@@ -64,7 +77,7 @@ def write_table_content(testset, metric, level):
     get_all_lang_correlations(mc, baseline, gold, system)
 
     # diversity (+5)
-    print(r'\midrule' + '\n' + r'\multirow{3}{*}{\pbox{1.5cm}{Diversity (+5)}}')
+    print(r'\midrule' + '\n' + r'\multirow{2}{*}{\pbox{1.9cm}{Diversity (+5)}}')
     system = system_prefix + '/laser/par' + metric + '-laser.num=5-' + level + 'level.tsv'
     print(r' & \laser ', end=' & ')
     get_all_lang_correlations(mc, baseline, gold, system)
@@ -73,7 +86,7 @@ def write_table_content(testset, metric, level):
     get_all_lang_correlations(mc, baseline, gold, system)
     
     # Output-specific (+1)
-    print(r'\midrule' + '\n' + r'\multirow{3}{*}{\pbox{1.5cm}{Output-specific (+1)}}')
+    print(r'\midrule' + '\n' + r'\multirow{2}{*}{\pbox{1.9cm}{Output-specific (+1)}}')
     system = system_prefix + '/mt-output-specific/par' + metric + '-constrained-laser.' + level + 'level.tsv'
     print(r' & \laser ', end=' & ')
     get_all_lang_correlations(mc, baseline, gold, system)
@@ -81,11 +94,56 @@ def write_table_content(testset, metric, level):
     print(r' & \treelstm ', end=' & ')
     get_all_lang_correlations(mc, baseline, gold, system)
 
+    
+    print(r'\midrule')
+    # constrained results (calculated externally)
+    if metric == 'bleu':
+        if level =='sys' and testset == 'newstest2019':
+            print(r'constraints & 4-grams  & 0.869 & 0.988 & 0.792 & 0.973 & 0.983 & 0.930 & 0.921 & 0.922\\')
+        elif level == 'seg' and testset == 'newstest2019':
+            print(r'constraints & 4-grams & 0.062 & 0.289 & 0.207 & 0.307 & 0.303 & 0.122 & 0.337 & 0.232 \\')
+        elif level == 'sys' and testset == 'newstest2018':
+            print(r'constraints & 4-grams \\')
+        elif level == 'seg' and testset == 'newstest2018':
+            print(r'constraints & 4-grams \\')
+    else:
+        print(r'constraints & 4-grams \\')    
+    
+
+    print(r'\midrule')
+    # best results (input manually)
+    if level == 'sys' and testset == 'newstest2019':
+        print(r'& WMT-19 best  & \textbf{0.950} & \textbf{0.995} & \textbf{0.993} & \textbf{0.998} & \textbf{0.989} & \textbf{0.979} & \textbf{0.988} & 0.985 \\')
+        print(r'& & \tiny (\textsc{YiSi-1\_SRL}) & \tiny (\textsc{METEOR}) & \tiny (\textsc{YiSi-0}) & \tiny (\textsc{WMDO}) & \tiny (\textsc{ESIM}) & \tiny (\textsc{YiSi-1}) & \tiny (\textsc{ESIM}) \\')
+        
+    elif level == 'seg' and testset == 'newstest2019':
+        print(r'& \tiny WMT-19 best  & \tiny\textbf{0.20} & \tiny\textbf{0.35} & \tiny\textbf{0.31} & \tiny\textbf{0.44} & \tiny\textbf{0.38} & \tiny\textbf{0.22}*** & \tiny\textbf{0.43} & 0.333 \\')
+        print(r'& & \tiny \tiny \textsc{YiSi-1$_{\text{SRL}}$} & \tiny \textsc{YiSi-1} & \tiny \textsc{YiSi-1} & \tiny \textsc{YiSi-1$_{\text{SRL}}$} & \tiny \textsc{YiSi-1$_{\text{SRL}}$} & \tiny \textsc{YiSi-1$_{\text{SRL}}$} & \tiny \textsc{YiSi-1$_\text{SRL}$} \\')
+
+    elif level == 'sys' and testset == 'newstest2018':
+        print(r'& WMT-18 best  &  0.981 & \textbf{0.997} & 0.991 & \textbf{0.996} & \textbf{0.995} & \textbf{0.970} & 0.982 \\')
+        print(r'& & \tiny (\textsc{RUSE})   & \tiny (\textsc{RUSE}) & \tiny (\textsc{WER}) & \tiny (\textsc{ITER}) & \tiny (\textsc{METEOR++}) & \tiny (\textsc{NIST}) & \tiny (\textsc{CDER}) \\')
+
+    elif level == 'seg' and testset == 'newstest2018':
+        print(r'& \tiny WMT-18 best  &  \tiny\textbf{0.35} & \tiny\textbf{0.50} & \tiny\textbf{0.37} & \tiny\textbf{0.27} & \tiny\textbf{0.31} & \tiny\textbf{0.26}*** & \tiny\textbf{0.22}  \\')
+        print(r' & & \tiny \textsc{RUSE} & \tiny \textsc{RUSE} & \tiny \textsc{RUSE} & \tiny \textsc{RUSE} & \tiny \textsc{RUSE} & \tiny \textsc{RUSE} & \tiny \textsc{RUSE} \\')
+
+
+        
+        
+def write_header():
+    print(r'\scalebox{1}{')
+    print(r'\centering\small')
+    print(r'\begin{tabular}{lllllllllll}')
+    print(r'\toprule')
+
+    
 def write_raw_table(testset, metric, level):
 
-
+    write_header()
     write_table_content(testset, metric, level)
-
+    print(r'\bottomrule')
+    print(r'\end{tabular}}')
 
 
 if __name__ == '__main__':
